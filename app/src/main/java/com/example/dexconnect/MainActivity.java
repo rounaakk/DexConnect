333ac903ac5e3a615966269a4c5a515664d6015e
Rounak <rounak122@gmail.com>
2020-06-14 17:28:27 +0530

    made minor changes

--- app/src/main/java/com/example/dexconnect/MainActivity.java
+++ app/src/main/java/com/example/dexconnect/MainActivity.java
@@ -1,10 +1,19 @@
 package com.example.dexconnect;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.Arrays;
+import java.util.List;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -13,12 +22,19 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView mDexList;
     CardsAdapter adapter;
 
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+
+    private static int RC_SIGN_IN = 1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mDexList=findViewById(R.id.myRecyclerView);
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mDexList = findViewById(R.id.myRecyclerView);
         mDexList.setLayoutManager(new LinearLayoutManager(this));
 
         FirebaseRecyclerOptions<Cards> options =
@@ -30,27 +46,57 @@ public class MainActivity extends AppCompatActivity {
 
         adapter = new CardsAdapter(options);
         mDexList.setAdapter(adapter);
-//        mDbRef = FirebaseDatabase.getInstance().getReference().child("Cards");
-//        mDbRef.keepSynced(true);
 
-//        mDexList.setHasFixedSize(true);
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
 
-//        mDexList.setAdapter(dex_cards);
-//
-//        ArtistArrayAdapter adapter = new ArtistArrayAdapter(this, artists);
-//        recyclerView = (RecyclerView) findViewById(R.id.cardList);
-//        recyclerView.setHasFixedSize(true);
-//        recyclerView.setAdapter(adapter);
-//        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+                if (user != null) {
 
-//        FirebaseRecyclerOptions<Cards> options =
-//                new FirebaseRecyclerOptions.Builder<Cards>()
-//                        .setQuery(FirebaseDatabase.getInstance()
-//                                .getReference()
-//                                .child("Cards"), Cards.class)
-//                        .build();
-//
-//        dex_cards = new CardsAdapter(options);
+                    Toast.makeText(MainActivity.this, "You are Signed in", Toast.LENGTH_SHORT).show();
+                } else {
+
+
+                    // Choose authentication providers
+                    List<AuthUI.IdpConfig> providers = Arrays.asList(
+                            new AuthUI.IdpConfig.EmailBuilder().build(),
+                            new AuthUI.IdpConfig.GoogleBuilder().build());
+
+                    // Create and launch sign-in intent
+                    startActivityForResult(
+                            AuthUI.getInstance()
+                                    .createSignInIntentBuilder()
+                                    .setIsSmartLockEnabled(false)
+                                    .setAvailableProviders(providers)
+                                    .build(),
+                            RC_SIGN_IN);
+
+
+                }
+            }
+
+
+        };
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,  Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode==RC_SIGN_IN){
+
+            if (resultCode==RESULT_OK)
+            {
+                Toast.makeText(this, "You are Signed in", Toast.LENGTH_SHORT).show();
+            }
+            else if (resultCode==RESULT_CANCELED)
+            {
+                Toast.makeText(this, "Sign In cancelled", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
     }
 
 
@@ -66,4 +112,17 @@ public class MainActivity extends AppCompatActivity {
         super.onStop();
         adapter.stopListening();
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mFirebaseAuth.removeAuthStateListener(mAuthStateListener);
+    }
+
 }
